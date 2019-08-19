2e7e52882a0f1cbe5e63c1ad460e8deaa315b96c
wizkids <wyzekid@gmail.com>
2019-08-19 19:52:31 +0300

    Tinkoff solution tests don't work

--- src/main/java/companies/tinkoff/Solution.java
+++ src/main/java/companies/tinkoff/Solution.java
@@ -2,42 +2,80 @@ package companies.tinkoff;
 
 public class Solution {
     public int[] sortedSquares(int[] A) {
-        int middle = 0;
-        boolean middleZero;
-        int[] result = new int[0];
-        for (int i = 0; i< A.length-1; i ++ ) {
+        int[] result = new int[A.length];
+        int MAX_INDEX = A.length - 1;
+        if (A.length == 0) {
+            return result;
+        }
+        if (A[0] >= 0) {
+            for (int i = 0; i <= MAX_INDEX; i++) {
+                result[i] = powSqr(A[i]);
+            }
+            return result;
+        }
+        if (A[MAX_INDEX] <= 0) {
+            for (int i = MAX_INDEX; i >= 0; i--) {
+                result[MAX_INDEX - i] = powSqr(A[i]);
+            }
+            return result;
+        }
+
+        int middleIndex = 0;
+        for (int i = 0; i <= MAX_INDEX - 1; i++) {
             if (A[i] == 0) {
-                middle = i;
+                middleIndex = i;
                 break;
             }
-            if (A[i] < 0 && A[i+1]> 0) {
-                if (Math.abs(A[i]) <= Math.abs(A[i+1])){
-                    middle = i;
+            if (A[i] < 0 && A[i + 1] > 0) {
+                if (Math.abs(A[i]) <= Math.abs(A[i + 1])) {
+                    middleIndex = i;
                 } else {
-                    middle = i+1;
+                    middleIndex = i + 1;
                 }
                 break;
             }
         }
-        middleZero = A[middle] == 0;
-        int left = middle;
-        int right = middle;
+        int leftIndex = middleIndex;
+        int rightIndex = middleIndex;
         int resultIndex = 0;
-        while (right <= A.length-1 || left >=0) {
-            if (middleZero) {
-                result[resultIndex] = 0;
+        while (leftIndex >= 0 || rightIndex <= MAX_INDEX) {
+            if (resultIndex == 0) {
+                result[resultIndex] = powSqr(A[middleIndex]);
                 resultIndex++;
                 continue;
             }
-            right++;
-            left--;
-            if (Math.abs(left) < Math.abs(right) && !middleZero) {
-                result[resultIndex] = A[left] * A[left];
-            } else if (Math.abs(left) > Math.abs(right) && !middleZero) {
-                result[resultIndex] = A[right] * A[right];
+            leftIndex--;
+            rightIndex++;
+            boolean leftIndexInBound = leftIndex >= 0;
+            boolean rightIndexInBound = rightIndex <= MAX_INDEX;
+            if (!leftIndexInBound) {
+                result[resultIndex] = powSqr(A[rightIndex]);
+                resultIndex++;
+                continue;
+            } else if (!rightIndexInBound) {
+                result[resultIndex] = powSqr(A[leftIndex]);
+                resultIndex++;
+                continue;
+            } else {
+                if (Math.abs(A[leftIndex]) < Math.abs(A[rightIndex])) {
+                    result[resultIndex] = powSqr(A[leftIndex]);
+                } else if (Math.abs(A[leftIndex]) > Math.abs(A[rightIndex])) {
+                    result[resultIndex] = powSqr(A[rightIndex]);
+                } else if (Math.abs(A[leftIndex]) == Math.abs(A[rightIndex])) {
+                    result[resultIndex] = powSqr(A[leftIndex]);
+                    resultIndex++;
+                    result[resultIndex] = powSqr(A[rightIndex]);
+                    leftIndex--;
+                    rightIndex++;
+                    continue;
+                }
             }
             resultIndex++;
         }
         return result;
+    }
+
+    private int powSqr(int number) {
+        return number * number;
     }
 }
---
+++ src/test/java/companies/tinkoff/SolutionTest.java
@@ -0,0 +1,33 @@
+package companies.tinkoff;
+
+import org.junit.Test;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void sortedSquaresTest() {
+        Solution solution = new Solution();
+        int[] input_1 = {};
+        int[] input_2 = {3};
+        int[] input_3 = {-3, -2, -1};
+        int[] input_4 = {0, 2, 5, 10};
+        int[] input_5 = {-7, -6, -2, 0, 1, 3};
+        int[] input_6 = {-5, -3, -2, 5, 8};
+
+        int[] expected_2 = {9};
+        int[] expected_3 = {1, 4, 9};
+        int[] expected_4 = {0, 4, 25, 100};
+        int[] expected_5 = {0, 1, 4, 9, 36, 49};
+        int[] expected_6 = {4, 9, 25, 25, 64};
+
+        assertArrayEquals(input_1, solution.sortedSquares(input_1));
+        assertArrayEquals(expected_2, solution.sortedSquares(input_2));
+        assertArrayEquals(expected_3, solution.sortedSquares(input_3));
+        assertArrayEquals(expected_4, solution.sortedSquares(input_4));
+        assertArrayEquals(expected_5, solution.sortedSquares(input_5));
+        assertArrayEquals(expected_6, solution.sortedSquares(input_6));
+    }
+}
